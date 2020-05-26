30c3fda1075dcc96b375356059b3729e02f399cf
Omar Khodr <omar.khoder2000@gmail.com>
2020-05-26 12:08:15 +0300

    Initial Commit

---
+++ Salli/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Salli
+//
+//  Created by Omar Khodr on 5/26/20.
+//  Copyright © 2020 Omar Khodr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
