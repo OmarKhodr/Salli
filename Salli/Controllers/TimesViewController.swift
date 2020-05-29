45ac8a9196c686150f00f47bd753faec22d213de
Omar Khodr <omar.khoder2000@gmail.com>
2020-05-29 12:08:53 +0300

    Started implementing Core Data in AppDelegate.swift and added Settings/Refresh buttons to UI

--- Salli/Controllers/TimesViewController.swift
+++ Salli/Controllers/TimesViewController.swift
@@ -11,7 +11,12 @@ import CoreLocation
 
 class TimesViewController: UIViewController {
     
-
+    
+    @IBOutlet weak var currentDateLabel: UILabel!
+    
+    @IBOutlet weak var refreshButton: UIButton!
+    @IBOutlet weak var settingsButton: UIButton!
+    
     @IBOutlet weak var fajrTimeLabel: UILabel!
     @IBOutlet weak var sunriseTimeLabel: UILabel!
     @IBOutlet weak var dhuhrTimeLabel: UILabel!
@@ -34,8 +39,26 @@ class TimesViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        prayerTimesBackgroundView.layer.cornerRadius = 12;
-        prayerTimesBackgroundView.layer.masksToBounds = true;
+        //Setting date label as current Hijri date
+        let dateFor = DateFormatter()
+
+        let hijriCalendar = Calendar.init(identifier: Calendar.Identifier.islamicCivil)
+        dateFor.locale = Locale.init(identifier: "en") // or "en" as you want to show numbers
+
+        dateFor.calendar = hijriCalendar
+
+        dateFor.dateFormat = "EEEE, MMM d, yyyy"
+        currentDateLabel.text = "\(dateFor.string(from: Date())) AH"
+        
+        //adding rounded corners to background of prayer times
+        prayerTimesBackgroundView.layer.cornerRadius = 12
+        prayerTimesBackgroundView.layer.masksToBounds = true
+        
+        refreshButton.layer.cornerRadius = 12
+        refreshButton.layer.masksToBounds = true
+        
+        settingsButton.layer.cornerRadius = 12
+        settingsButton.layer.masksToBounds = true
         
         // setting view as delegate for location manager
         locationManager.delegate = self
@@ -89,6 +112,34 @@ extension TimesViewController: PrayerTimesManagerDelegate {
         formatter.dateFormat = "h:mm a"
         //dictionary of dates in model containing datetimes for prayers
         let dict = model.times
+        
+        let currTime = Date().time
+        var currLabel: UILabel?
+        var currTimeLabel: UILabel?
+        switch currTime {
+        case dict[K.fajr]!.time..<dict[K.sunrise]!.time:
+            currLabel = self.fajrLabel
+            currTimeLabel = self.fajrTimeLabel
+            break
+        case dict[K.dhuhr]!.time..<dict[K.asr]!.time:
+            currLabel = self.dhuhrLabel
+            currTimeLabel = self.dhuhrTimeLabel
+            break
+        case dict[K.asr]!.time..<dict[K.maghrib]!.time:
+            currLabel = self.asrLabel
+            currTimeLabel = self.asrTimeLabel
+            break
+        case dict[K.maghrib]!.time..<dict[K.isha]!.time:
+            currLabel = self.maghribLabel
+            currTimeLabel = self.maghribTimeLabel
+            break
+        case dict[K.isha]!.time..<Time(23, 59):
+            currLabel = self.ishaLabel
+            currTimeLabel = self.ishaTimeLabel
+            break
+        default:
+            break
+        }
         //calling main thread asynchronously to update UI elements
         DispatchQueue.main.async {
             self.fajrTimeLabel.text = formatter.string(from: dict[K.fajr]!)
@@ -97,29 +148,9 @@ extension TimesViewController: PrayerTimesManagerDelegate {
             self.asrTimeLabel.text = formatter.string(from: dict[K.asr]!)
             self.maghribTimeLabel.text = formatter.string(from: dict[K.maghrib]!)
             self.ishaTimeLabel.text = formatter.string(from: dict[K.isha]!)
-            let currTime = Date().time
-            var currLabel: UILabel?
-            switch currTime {
-            case dict[K.fajr]!.time..<dict[K.sunrise]!.time:
-                currLabel = self.fajrLabel
-                break
-            case dict[K.dhuhr]!.time..<dict[K.asr]!.time:
-                currLabel = self.dhuhrLabel
-                break
-            case dict[K.asr]!.time..<dict[K.maghrib]!.time:
-                currLabel = self.asrLabel
-                break
-            case dict[K.maghrib]!.time..<dict[K.isha]!.time:
-                currLabel = self.maghribLabel
-                break
-            case dict[K.isha]!.time..<Time(23, 59):
-                currLabel = self.ishaLabel
-                break
-            default:
-                break
-            }
-            if let currLabel = currLabel {
+            if let currLabel = currLabel, let currTimeLabel = currTimeLabel {
                 currLabel.textColor = #colorLiteral(red: 0.1450980392, green: 0.5137254902, blue: 0.8549019608, alpha: 1)
+                currTimeLabel.textColor = #colorLiteral(red: 0.1450980392, green: 0.5137254902, blue: 0.8549019608, alpha: 1)
             }
         }
     }
